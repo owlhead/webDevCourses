604ba06a517bd585842a0ebfd8711d8780302610
owlhead <owlhead>
2019-05-04 21:09:11 +0200

    added navigation bar!

---
+++ mod3_solution/js/script.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    var para_ = document.getElementsByClassName('content_text');
+    var file = new XMLHttpRequest();
+    file.open('GET', 'lorem_ipsum_600.txt', true);
+    file.onreadystatechange = function() {
+        if (file.readyState === 4 ) { // make sure the document is ready to parse
+            var text_ = file.responseText;
+            
+            text_ = text_.split('\n').join('<br>');
+
+            for (i = 0; i < para_.length; i++){
+                para_[i].innerHTML = text_;
+            }
+        }
+    };
+    file.send(null);
+};
